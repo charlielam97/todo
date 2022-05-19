188135a305a95c5aa52db59ebd3c4f9ad6916867
Charlie Lam <46893337+charlielam97@users.noreply.github.com>
2022-05-19 21:49:21 +0200

    first commit

---
+++ ToDoList.js
@@ -0,0 +1,80 @@
+let add = document.getElementById("addToDo");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField");
+const value = inputField.value;
+
+let taskList = [];
+let storedTasks = JSON.parse(localStorage.taskList ?? '[]');
+
+var clearStorage = document.getElementById('clearStorage');
+clearStorage.onclick = function() {
+  localStorage.clear();
+  console.log(localStorage);
+}
+
+function addTodos() {
+  taskList = storedTasks;
+  for (let i = 0; i < storedTasks.length; i++) {
+    let paragraph = document.createElement("p");
+    paragraph.classList.add("paragraph-styling");
+
+    paragraph.innerText = storedTasks[i];
+    toDoContainer.appendChild(paragraph);
+
+    paragraph.addEventListener("click", function () {
+      paragraph.style.textDecoration = "line-through";
+    });
+
+    paragraph.addEventListener("dblclick", function () {
+      toDoContainer.removeChild(paragraph);
+      for (let i = 0; i < taskList.length; i++) {
+        //If we doubleclick we remove it from the the tasklist
+        if (taskList[i] === paragraph.innerText) {
+          taskList.splice(i, 1);
+          break;
+        } else {
+          continue;
+        }
+      }
+      taskList = storedTasks;
+      localStorage.setItem("taskList", JSON.stringify(taskList));
+    });
+  }
+}
+
+add.addEventListener("click", function () {
+  let paragraph = document.createElement("p");
+  paragraph.classList.add("paragraph-styling");
+
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = " ";
+
+  taskList.push(paragraph.innerText);
+  localStorage.setItem("taskList", JSON.stringify(taskList));
+  console.log(localStorage);
+
+  paragraph.addEventListener("click", function () {
+    paragraph.style.textDecoration = "line-through";
+  });
+
+  paragraph.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(paragraph);
+    for (var i = 0; i < taskList.length; i++) {
+      if (taskList[i] === paragraph.innerText) {
+        taskList.splice(i, 1);
+        break;
+      } else {
+        continue;
+      }
+    }
+    //taskList = storedTasks;
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+    console.log("-------------------------------------");
+    console.log(localStorage);
+  });
+});
+
+// Goes live at the beginning of the program
+
+addTodos();
